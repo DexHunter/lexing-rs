8723cd159cbf64774c6b8c6b3f713f1979a9c800
Xie Yuheng <xyheme@gmail.com>
2018-10-20 18:34:07 +0800

    fix ci

--- lexing/src/lib.rs
+++ lexing/src/lib.rs
@@ -1,9 +1,57 @@
+#![feature (uniform_paths)]
 
+#![allow (unused_parens)]
+#![allow (dead_code)]
+#![allow (unused_macros)]
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+use std::collections::HashSet;
+
+#[derive (Clone)]
+#[derive (Debug)]
+#[derive (PartialEq, Eq)]
+pub struct CharTable {
+    pub quotation_mark_vec: HashSet <char>,
+    pub char_vec: HashSet <char>,
+}
+
+#[derive (Clone)]
+#[derive (Debug)]
+#[derive (PartialEq, Eq)]
+pub enum Token {
+    Quote {
+        quotation_mark: char,
+        string: String,
+        span: Span,
+    },
+    Word {
+        word: String,
+        span: Span,
+    },
+    Char {
+        ch: char,
+        span: Span,
+    },
+}
+
+#[derive (Clone)]
+#[derive (Debug)]
+#[derive (PartialEq, Eq)]
+pub struct Span {
+    pub lo: usize,
+    pub hi: usize,
+}
+
+#[derive (Clone)]
+#[derive (Debug)]
+#[derive (PartialEq, Eq)]
+pub struct LexingError;
+
+#[test]
+fn test_lexing () -> Result<(), LexingError> {
+    Ok (())
+}
+
+#[test]
+fn play () {
+
 }
